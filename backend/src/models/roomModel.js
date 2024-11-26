865fda5366dfba129b42396396d0e4855e9994e9
JJrock001 <jjmaleewong@gmail.com>
2024-11-26 16:11:00 +0700

    a

---
+++ backend/src/models/roomModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+  room_name: {
+    type: String,
+    required: true,
+  },
+  room_id: {
+    type: Number,
+    required: true,
+  },
+  room_avl: {
+    type: [Boolean],  
+    required: true,
+  },
+});
+
+const Room = mongoose.model("Room", roomSchema);
+
+export default Room;
