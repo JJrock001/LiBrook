e01175604664246e98ff412e0c38b16e8d180601
JJrock001 <jjmaleewong@gmail.com>
2024-11-26 14:44:59 +0700

    isLogin

--- frontend/public/scripts/nomain.js
+++ frontend/public/scripts/nomain.js
@@ -1,7 +1,9 @@
 const USERNAME = "admin";
 const PASSWORD = "password123";
+isLogin = false;
 
 function login() {
+  
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   const errorMessage = document.getElementById("error-message");
@@ -12,6 +14,7 @@ function login() {
   if (username === USERNAME && password === PASSWORD) {
     alert("Login successful!");
     window.location.href = "reservation.html";
+    isLogin = true;
   } else {
     errorMessage.style.display = "block";
   }
